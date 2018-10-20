7ed99a342105eb82c91d948df32c44add7af3abf
Leo <leo.baltazor@gmail.com>
2018-10-21 00:31:48 +0300

    init

--- src/app/custom-material.module.ts
+++ src/app/custom-material.module.ts
@@ -1,21 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatButtonModule,
-  MatCheckboxModule
-} from '@angular/material';
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 
-const MODULES = [
-  BrowserAnimationsModule,
-  MatButtonModule,
-  MatCheckboxModule
-];
+const MODULES = [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule];
 @NgModule({
-  imports: [
-    ...MODULES
-  ],
-  exports: [
-    ...MODULES
-  ],
+  imports: [...MODULES],
+  exports: [...MODULES]
 })
-export class CustomMaterialModule { }
+export class CustomMaterialModule {}
