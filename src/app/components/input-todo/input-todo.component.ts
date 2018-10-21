779a1d5e69d01de2d04aa662db271cf189514a25
Leo <leo.baltazor@gmail.com>
2018-10-22 02:22:22 +0300

    start tuning input

--- src/app/components/input-todo/input-todo.component.ts
+++ src/app/components/input-todo/input-todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Todo } from "../../todo";
+import { debounceTime, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-input-todo",
@@ -9,7 +10,7 @@ import { Todo } from "../../todo";
 })
 export class InputTodoComponent implements OnInit {
   todoItem = new FormGroup({
-    description: new FormControl(""),
+    description: new FormControl("", [Validators.minLength(3), Validators.required]),
     is_checked: new FormControl(false)
   });
   @Output()
@@ -17,9 +18,12 @@ export class InputTodoComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // this.todoItem.valueChanges.subscribe(value => {
-    //   this.newAddedItem = value;
-    // });
+    this.todoItem.valueChanges.pipe(
+      debounceTime(500)
+    )
+    .subscribe(value => {
+      console.log(value);
+    });
   }
   onSubmit() {
     this.newAddedItem.emit(this.todoItem.value as Todo);
