ee284f08a398c7f7395c6ecfc167ebcac2eff022
Leo <leo.baltazor@gmail.com>
2018-10-22 00:56:37 +0300

    add bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { InputTodoComponent } from "./components/input-todo/input-todo.component";
 import { ItemTodoComponent } from "./components/item-todo/item-todo.component";
 import { ReactiveFormsModule } from "@angular/forms";
-import { AppTodoComponent } from './app-todo/app-todo.component';
+import { AppTodoComponent } from "./app-todo/app-todo.component";
 
 @NgModule({
   declarations: [
